87bf6750ff8579a069cc6f5730b93e4114ef3f5f
ilnikolay <94624457+ilnikolay@users.noreply.github.com>
2021-11-26 17:33:51 +0200

    Update linux_is_cool.sh

--- linux/lab04/linux_is_cool.sh
+++ linux/lab04/linux_is_cool.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-grep -Ilri 'http' /etc/* 2> /dev/null | xargs sudo du | sort -rg | head -1 | awk '{print $2}' | xargs sed '13clinux is cool' | head -15
+grep -irl http /etc 2>/dev/null | xargs file | grep ", ASCII text" | cut -d: -f1 | xargs du | sort -rg | head -1 | awk '{print $2}' | xargs sed '13clinux is cool' | head -15
 
 #second option
 find /etc/ -type f -printf '%s %p\n' 2> /dev/null | sort -n | cut -d" " -f2 | xargs grep -Ili 'http' 2> /dev/null | tail -1 | xargs sed '13clinux is cool' | head -15
