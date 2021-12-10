6296d56deedbf7a35c7e1495866911512da8364b
Nikolay Ninov <nikolayninov@Nikolays-MBP.net1.bg>
2021-12-10 18:10:28 +0200

    fixed task1 of lab06

--- Docker_VM/lab06/task1.sh
+++ Docker_VM/lab06/task1.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#set -x
 
 # Create networks
 for i in {1..2}; do
@@ -6,17 +7,25 @@ for i in {1..2}; do
 done
 
 # Create containers
-docker run -dit --name alpine1 --net=net1 alpine 
-docker run -dit --name alpine2 --net=net1 alpine 
-docker run -dit --name alpine3 --net=net2 alpine 
+for j in {1..3}; do
+  docker run -dit --name alpine$j  alpine 
+done
+
+ip_alpine1=`docker inspect alpine1 -f "{{.NetworkSettings.IPAddress}}"`
+ip_alpine2=`docker inspect alpine2 -f "{{.NetworkSettings.IPAddress}}"`
+ip_alpine3=`docker inspect alpine3 -f "{{.NetworkSettings.IPAddress}}"`
 
 # Add containers to different networks
+docker network connect net1 alpine1
+docker network connect net1 alpine2
 docker network connect net2 alpine2
+docker network connect net2 alpine3
+
 
 # Performing pings
 echo "Ping from alpine1 to alpine2"
-docker exec alpine1 ping -c2 alpine2
+docker exec alpine1 ping -c2 $ip_alpine2
 echo "Ping from alpine1 to alpine3"
-docker exec alpine1 ping -c2 alpine3
+docker exec alpine1 ping -c2 $ip_alpine3
 echo "Ping from alpine2 to alpine3"
-docker exec alpine2 ping -c2 alpine3
+docker exec alpine2 ping -c2 $ip_alpine3
